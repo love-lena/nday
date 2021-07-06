0a4f624f4caee9ab8e38fda7bd7a8c243e3e4711
Lena <lena.hicksonlong@gmail.com>
2021-07-06 16:33:12 -0700

    Launch code after create
    Resolves: #4

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use chrono::prelude::*;
 use chrono::Local;
 use std::fs::File;
-use std::fs::OpenOptions;
+//use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::path::Path;
+use std::process::Command;
 //use chrono::format;
 
 static TEMPLATE_TEXT: &str =
@@ -38,36 +39,8 @@ fn main() {
         }
     };
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    // let mut ready_file = match File::open(&path) {
-    //     Err(_why) => match File::create(&path) {
-
-    //         Err(why) => panic!("couldn't create {}: {}", display, why),
-
-    //         Ok(file) => match file.write_all(TEMPLATE_TEXT.as_bytes()) {
-    //                 Err(why) => panic!("couldn't write to {}: {}", display, why),
-    //                 Ok(_) => {println!("successfully wrote to {}", display);file;},
-    //             }
-    //     },
-    //     Ok(existing_file) => existing_file,
-    // };
-
-    // // Open a file in write-only mode, returns `io::Result<File>`
-    // let mut file = match File::create(&path) {
-    //     Err(why) => panic!("couldn't create {}: {}", display, why),
-    //     Ok(file) => file,
-    // };
-
-    // // Open a file in write-only mode, returns `io::Result<File>`
-    // let file_option = OpenOptions::new().read(true).write(true).create(true).open(&path);
-    // let file = match file_option {
-    //     Err(why) => panic!("couldn't create {}: {}", display, why),
-    //     Ok(file) => file,
-    // };
-
-    // // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-    // match file.write_all(TEMPLATE_TEXT.as_bytes()) {
-    //     Err(why) => panic!("couldn't write to {}: {}", display, why),
-    //     Ok(_) => println!("successfully wrote to {}", display),
-    // }
+    match Command::new("code").arg(path_str).output() {
+        Err(_) => panic!("could not execute code, is it configured correctly?"),
+        Ok(_) => (),
+    }
 }
