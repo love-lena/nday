62387817b374462a630223211e21aa2f1efddfa4
Lena <lena.hicksonlong@gmail.com>
2021-07-05 18:01:04 -0700

    Create template file
    Resolves: #1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+static TEMPLATE_TEXT: &str = "DATE\n\nTo-do today:\n- \n\nDone today:\n- \n\nKicked to tomorrow:\n- \n";
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("today.txt");
+    let display = path.display();
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
+    match file.write_all(TEMPLATE_TEXT.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
 }
