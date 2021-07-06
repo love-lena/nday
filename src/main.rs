f18a6c1b937b2436b6c5bf706541477800765d26
Lena <lena.hicksonlong@gmail.com>
2021-07-06 15:20:22 -0700

    Check for existing file
    Resolves: #3

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,46 @@ static TEMPLATE_TEXT: &str =
 
 fn main() {
     let local: Date<Local> = Local::today();
-    println!("{}", local.format("%b%-e").to_string());
+    //println!("{}", local.format("%b%-e").to_string());
 
     let path_str = local.format("%b%-e.txt").to_string();
     let path = Path::new(&path_str);
     let display = path.display();
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&path) {
-        Err(_why) => match File::create(&path) {
-            Err(why) => panic!("couldn't create {}: {}", display, why),
-            Ok(file) => file,
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let file = match File::open(&path) {
+        Err(_) => match File::create(&path) {
+            Err(why) => {
+                panic!("couldn't write to {}: {}", display, why)
+            }
+            Ok(mut new_file) => {
+                match new_file.write_all(TEMPLATE_TEXT.as_bytes()) {
+                    Err(why) => panic!("couldn't write to {}: {}", display, why),
+                    Ok(_) => println!("created today's notes {}", display),
+                };
+                new_file
+            }
         },
-        Ok(file) => file,
+
+        Ok(file) => {
+            println!("opened today's notes {}", display);
+            file
+        }
     };
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    // let mut ready_file = match File::open(&path) {
+    //     Err(_why) => match File::create(&path) {
+
+    //         Err(why) => panic!("couldn't create {}: {}", display, why),
+
+    //         Ok(file) => match file.write_all(TEMPLATE_TEXT.as_bytes()) {
+    //                 Err(why) => panic!("couldn't write to {}: {}", display, why),
+    //                 Ok(_) => {println!("successfully wrote to {}", display);file;},
+    //             }
+    //     },
+    //     Ok(existing_file) => existing_file,
+    // };
 
     // // Open a file in write-only mode, returns `io::Result<File>`
     // let mut file = match File::create(&path) {
